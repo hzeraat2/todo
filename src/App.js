233d255a25ea8de19db748dca7c5a5c0eb6c9256
Hamed Zeraatpisheh <hzeraat2@yahoo.com>
2022-12-06 15:37:53 +0000

    First working ITV tech test app.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Input from './Input';
+import './App.css';
+ 
+function App() {
+
+ return (
+  <div className='App'>
+   <header className='App-header'>
+    	To Do List
+   </header>
+   <Input/>
+  </div>
+ );
+}
+ 
+export default App;
---
+++ src/Input.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+
+    const loadPreviousTasks = localStorage.getItem('todoList');
+    const [input, setInput] = useState('');
+    const [list, setList] = useState(loadPreviousTasks.split(',') || []);
+
+    const addToList = (newItem) => {
+        setList(current => [...current, newItem]);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        addToList(event.target[0].value);
+        setInput('');
+    }
+
+    const handleChange = (event) => {
+        setInput(event.target.value);
+    }
+
+    window.addEventListener("beforeunload", (ev) => {
+        if (list) localStorage.setItem('todoList', list);
+    });
+
+    const removeAllTasks = () => {
+        setList([]);
+    } 
+
+    return (
+        <div className='form'>
+            <form onSubmit={handleSubmit} className=''>
+                <label>
+                    New task:
+                    <input type="text" value={input} onChange={handleChange} className='button'/>
+                </label>
+                <input type="submit" value="Submit" disabled={input === ''} className='button'/>
+            </form>
+            {
+                list && list.map((task, index) => <div className='button' style={{backgroundColor: (index % 2 == 0)? 'lightgrey' : 'lightblue'}} key={index}>{task}</div>)
+            }
+
+            <input type="submit" value="Remove All Tasks" onClick={removeAllTasks} className='button'/>
+        </div>
+    );
+};
+
+export default Header;
